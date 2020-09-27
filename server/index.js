064eb3bac117ac13757539b8db47043f35c04fe6
NoriX <versatility.95@mail.ru>
2020-09-27 17:51:20 +0300

    MongoDB connected, Fetching data from database with Queryes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
+const keys = require('./config/keys');
+
+const mongoParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
+mongoose.connect(keys.mongoURI, mongoParams, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Database connected successfully');
+});
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
