c920243a8c57d2562bb579e07b9fb02eeaae2530
NoriX <versatility.95@mail.ru>
2020-10-03 20:08:12 +0300

    DirectorsForm -> functional component && saving directors (mutations)

--- client/src/components/DirectorsForm/DirectorsForm.js
+++ client/src/components/DirectorsForm/DirectorsForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useMutation } from '@apollo/client';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import DialogTitle from '@material-ui/core/DialogTitle';
@@ -8,51 +9,56 @@ import SaveIcon from '@material-ui/icons/Save';
 import { withStyles } from '@material-ui/core/styles';
 import { styles } from './styles';
 
-class DirectorsForm extends React.Component {
-  handleClose = () => { this.props.onClose(); };
+import { addDirectorMutation } from './mutations';
+import { directorsQuery } from '../DirectorsTable/queries';
 
-  handleSave = () => {
-    const { selectedValue, onClose } = this.props;
+const DirectorsForm = props => {
+
+  const [saveDirector] = useMutation(addDirectorMutation);
+  const { classes, open, handleChange, selectedValue = {} } = props;
+  const { name, age } = selectedValue;
+
+  const handleSave = () => {
+    const { selectedValue, onClose } = props;
     const { id, name, age } = selectedValue;
+    saveDirector({
+      variables: { name, age: Number(age) },
+      refetchQueries: [{ query: directorsQuery }]
+    });
     onClose();
   };
 
-  render() {
-    const { classes, open, handleChange, selectedValue = {} } = this.props;
-    const { name, age } = selectedValue;
-
-    return (
-      <Dialog onClose={this.handleClose} open={open} aria-labelledby="simple-dialog-title">
-        <DialogTitle className={classes.title} id="simple-dialog-title">Director information</DialogTitle>
-        <form className={classes.container} noValidate autoComplete="off">
-          <TextField
-            id="outlined-name"
-            label="Name"
-            className={classes.textField}
-            value={name}
-            onChange={handleChange('name')}
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-rate"
-            label="Age"
-            className={classes.textField}
-            value={age}
-            onChange={handleChange('age')}
-            type="number"
-            margin="normal"
-            variant="outlined"
-          />
-          <div className={classes.wrapper}>
-            <Button onClick={this.handleSave} variant="contained" color="primary" className={classes.button}>
-              <SaveIcon /> Save
+  return (
+    <Dialog onClose={() => { props.onClose() }} open={open} aria-labelledby="simple-dialog-title">
+      <DialogTitle className={classes.title} id="simple-dialog-title">Director information</DialogTitle>
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="outlined-name"
+          label="Name"
+          className={classes.textField}
+          value={name}
+          onChange={handleChange('name')}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-rate"
+          label="Age"
+          className={classes.textField}
+          value={age}
+          onChange={handleChange('age')}
+          type="number"
+          margin="normal"
+          variant="outlined"
+        />
+        <div className={classes.wrapper}>
+          <Button onClick={handleSave} variant="contained" color="primary" className={classes.button}>
+            <SaveIcon /> Save
             </Button>
-          </div>
-        </form>
-      </Dialog>
-    );
-  }
+        </div>
+      </form>
+    </Dialog>
+  );
 };
 
 export default withStyles(styles)(DirectorsForm);
